fd85db7621d9a212339f6badfee3a0e876a2968c
Daniel Ko <danko56666@gmail.com>
2019-03-14 11:41:47 -0400

    google oauth setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions/auth";
+
+import Landing from "./Landing";
 
 class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
     render() {
         return (
             <div>
-                The best Collab Site you've ever seen. It doesn't even have any
-                functionality.
+                <BrowserRouter>
+                    <Route exact path="/" component={Landing} />
+                </BrowserRouter>
             </div>
         );
     }
 }
 
-export default App;
+export default connect(
+    null,
+    { fetchUser }
+)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
 
-const store = createStore(() => {}, {}, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
