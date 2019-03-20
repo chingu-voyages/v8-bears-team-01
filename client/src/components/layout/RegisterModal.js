5fd9cab04102685e78f46f040814717fd11f21a4
Crystal <hello@crystalbohacek.com>
2019-03-19 20:05:13 -0400

    updated styling of modal

--- client/src/components/layout/RegisterModal.js
+++ client/src/components/layout/RegisterModal.js
@@ -14,19 +14,21 @@ const RegisterModal = (props) => (
         ariaHideApp={false}
         closeTimeoutMS={300}
       >
-       <button className="btn btn-link nav-link text-muted close-button" onClick={props.handleRequestClose}>x</button>
+       <div className="modal-header">
+          <button className="btn btn-link nav-link text-muted close-button" onClick={props.handleRequestClose}>x</button>
+            <h2 className="font-weight-light modal-text">Sign Up</h2>
+        </div>
         <div className="content">
-          <h2 className="display-5 text-center font-weight-light mb-4">Sign Up</h2>
-          <a className="btn google-btn btn-block" href="/auth/google">
-            <i className="fab fa-google modal-icon"></i>  Sign up With Google
+          <a className="btn google-btn mr-2 ml-2" href="/auth/google">
+            <i className="fab fa-google modal-icon"></i>
           </a>
-          <a className="btn facebook-btn btn-block" href="/auth/facebook">
-            <i className="fab fa-facebook modal-icon"></i>  Sign up With Facebook
+          <a className="btn facebook-btn mr-2 ml-2" href="/auth/facebook">
+            <i className="fab fa-facebook modal-icon"></i>
           </a>
           <p>or</p>
-          <input placeholder="Email"></input><br></br>
-          <input placeholder="Password"></input><br></br>
-          <button>Sign Up</button>
+          <input className="form-control" placeholder="Email"></input><br></br>
+          <input className="form-control" placeholder="Password"></input><br></br>
+          <button className="btn sign-up-button mb-4">Sign Up</button>
           <p className="policy-modal-link text-secondary"><span>Read our </span>
             <button className="btn btn-link policy-modal-link" onClick={props.handleRequestClose}><Link to="/privacy">Privacy Policy.</Link></button>
           </p>
