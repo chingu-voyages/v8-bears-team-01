eb964427a681d6fc94b8c7c5c6229df417e7c028
Crystal <hello@crystalbohacek.com>
2019-03-29 18:16:03 -0400

    filter results

--- routes/search.js
+++ routes/search.js
@@ -3,14 +3,13 @@ const Project = require("../models/Projects");
 
 module.exports = app => {
   app.get("/search", (req, res) => {
-    console.log("Search term: " + req.query.q);
-  //   const searchQuery = new RegExp(req.query.q, 'i');
-
-  //   Project.find().or([
-  //       { 'name': { $regex: searchQuery }},
-  //       { 'description': { $regex: searchQuery }},
-  //   ]).sort('name').exec(function(err, data) {
-  //       res.render("projects/search", {config: config, term: req.query.term, projects: data});
-  //   });  
+    const searchQuery = new RegExp(req.query.q);
+    
+  Project.find().or([
+        { 'name': { $regex: searchQuery }},
+        { 'description': { $regex: searchQuery }},
+    ]).sort('name').exec(function(err, data) {
+        res.json(data);
+    });  
   });
 };
