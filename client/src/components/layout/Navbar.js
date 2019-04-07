95ac53e50caf1b6b00f4fbad799daac205037a0b
Crystal <hello@crystalbohacek.com>
2019-04-06 22:01:27 -0400

    changed background navbar and search bar to dark theme

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -197,9 +197,9 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-black">
           <h5 className="my-0 mr-md-auto font-weight-normal">
-            <Link className="text-dark" to="/">
+            <Link className="text-light" to="/">
               CodeCollab
             </Link>
           </h5>
