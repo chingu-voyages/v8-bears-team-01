4a30419893f410eb13183e178915806e461939e8
Crystal <hello@crystalbohacek.com>
2019-04-06 23:31:06 -0400

    updated card styling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,7 +7,7 @@ const Project = props => (
       className="col-sm-12 col-md-6 col-lg-3 pb-3"
       key={props.project._id}
     >
-      <div className="card m-0">
+      <div className="card text-white bg-secondary m-0 mb-4">
           <Link to={"/project/:" + props.project._id}>
               {" "}
               <img
@@ -16,16 +16,12 @@ const Project = props => (
                   alt="Card image cap"
               />{" "}
           </Link>
-          <div className="card-body">
-              <h5 className="card-title">{props.project.projectName}</h5>
+          <div class="card-body">
+          <h5 class="card-title">{props.project.projectName}</h5>
+          <p class="card-text">{props.project.description}</p>
           </div>
-          <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                  Open Positions: {props.project.num_open_positions}
-              </li>
-              <li className="list-group-item">Team Leader: {props.project.ownerName}</li>
-          </ul>
       </div>
+      
     </div>
 );
 
