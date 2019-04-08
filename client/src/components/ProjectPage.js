5b80d2376d91f1ad3d69355f2cc2463ace0ec913
Crystal <hello@crystalbohacek.com>
2019-04-08 17:31:17 -0400

    basic styling

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -21,11 +21,21 @@ class ProjectPage extends Component {
     const { project } = this.state;
     return(
       <div>
-        <div className="container">
-          <h2 className="mt-4 mb-4">{project.projectName}</h2>
-          <p>Project Owner: {!!project.ownerName && project.ownerName}</p>
-          <p>Deadline: {!!project.deadline && project.deadline}</p>
-          <p>Description: {!!project.description && project.description}</p>
+        <div className="py-5 text-white">
+          <div className="mx-auto col-md-10">
+            <div className="project-container row">
+              <div className="col-sm-8 col-md-8 col-lg-8">
+                <img className="project-page-img" src="https://i.imgur.com/nZ22mf9.jpg"></img>
+              </div>
+              <div className="col-sm-4 col-md-4 col-lg-4">
+                <h1 className="text-light">{project.projectName}</h1>
+                <p className="mb-4 lead text-light">Created by: {!!project.ownerName && project.ownerName}</p> 
+                <p>Deadline: {!!project.deadline && project.deadline}</p>
+                <p>Description: {!!project.description && project.description}</p>
+                <button className="btn btn-teal">Apply to Project</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
