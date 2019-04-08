18deb83d4fdb9aba8a6e5e162b1b74df9340f5a6
Crystal <hello@crystalbohacek.com>
2019-04-08 13:59:55 -0400

    display project data

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 
 class ProjectPage extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      project: []
+    };
+  }
   getProject(projectId){
-    //TODO: fetch request to /projects/:projectId
-    console.log(projectId)
+    fetch(`/api/projects/${projectId}`)
+    .then(response => response.json())
+    .then(json => this.setState({ project: json }))
+    .catch(err => console.log(err));
   }
   componentDidMount(){
-    const projectId = this.props.match.params.id;
+    const projectId = this.props.match.params.id.replace(":", "");
     this.getProject(projectId);
   }
   render(){
+    const { project } = this.state;
     return(
       <div>
-        <p>This is the project page.</p>
+        <div className="container">
+          <h2 className="mt-4 mb-4">{project.projectName}</h2>
+          <p>Project Owner: {!!project.ownerName && project.ownerName}</p>
+          <p>Deadline: {!!project.deadline && project.deadline}</p>
+          <p>Description: {!!project.description && project.description}</p>
+        </div>
       </div>
     )
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,8 @@
 const Project = require("../models/Projects");
 
 module.exports = app => {
+
+    //get all projects
     app.get("/api/projects", async (req, res) => {
         try {
           const projects = await Project.find();
@@ -11,6 +13,7 @@ module.exports = app => {
       }
     });
 
+    //create a project
     app.post("/api/projects", async (req, res) => {
         const {
             ownerName,
@@ -37,5 +40,21 @@ module.exports = app => {
         } catch (err) {
             res.status(422).send(err);
         }
+    });
+
+    //get a single project 
+    app.get("/api/projects/:projectId", (req, res, next) => {
+      Project.findById(req.params.projectId)
+        .then(project => {
+          if (project) {
+            res.status(200).json(project);
+          } else {
+            res.status(404).json("No project found with this id");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(404).json({ error: err });
+        });
     });
 };
