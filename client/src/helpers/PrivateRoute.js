7d3d54cb82caec8fdbd3f3487e92d57ab17c13e8
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-10 18:37:28 -0500

    added private route so only authed user can access routes

---
+++ client/src/helpers/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+
+export const PrivateRoute = ({component: Component,auth,...rest}) => (
+  <Route 
+  {...rest}
+  
+  render={props =>
+    
+    auth ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+        to={{
+          pathname: "/",
+          state: { from: props.location }
+        }}
+        />
+      )
+    }
+/>
+)
+
+
+
+
