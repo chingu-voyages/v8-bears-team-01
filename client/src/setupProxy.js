e39477fdafd0ecd9fa6be1e5cf16c61d48c73f89
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-10 23:33:07 -0500

    added logout route to proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,5 +7,6 @@ module.exports = function(app) {
     app.use(proxy("/auth/facebook", { target: "http://localhost:5000" }));
     app.use(proxy("/auth/*", { target: "http://localhost:5000" }));
     app.use(proxy("/api/user/*", { target: "http://localhost:5000" }));
+    app.use(proxy("/api/logout",{target: "http://localhost:5000"}))
 
 };
