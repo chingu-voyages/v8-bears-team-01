33fc3cb8cb280c19d4e2e41073cff8ceec44bed6
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-10 23:36:24 -0500

    added verify tokek middleware

---
+++ middlewares/verityToken.js
@@ -0,0 +1,24 @@
+const verifyToken = (req,res,next) =>{
+  const bearerHeader = req.headers['authorization'];
+
+  //check of bearer is undefined
+  if(typeof bearerHeader !=='undefined'){
+    const bearer = bearerHeader.split(' ');
+
+    //get token from array
+    const bearerToken = bearer[1]
+
+    //ser the token
+    req.token = bearerToken
+
+    next();
+  } else {
+    res.sendStatus(403)
+  }
+
+}
+
+
+module.exports = {
+  verifyToken
+}
