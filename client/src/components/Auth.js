71104535459e2ef92a4bf1dd82b6552e433a0282
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-11 17:32:17 -0500

    added a component to handle redirect from server

---
+++ client/src/components/Auth.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import { withRouter } from "react-router";
+
+import { connect } from "react-redux";
+
+
+import { get_auth_token } from "../actions/auth"
+
+ class Auth extends Component {
+
+  componentDidMount(){
+    this.props.get_auth_token()
+        .then(()=>{
+          localStorage.setItem('authToken',this.props.token)
+          this.props.history.push("/dashboard")
+        })
+  }
+
+  render() {
+    return (
+      <>
+        <p>redirecting...</p>
+      </>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+      token: state.auth.token
+  };
+}
+
+export default withRouter(connect(mapStateToProps,{get_auth_token})(Auth))
