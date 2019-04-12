8dec5c33710995e219632a685bc2bf2d5f102130
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-12 18:07:55 -0500

    removed connect and other unused code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { fetchUser } from "../actions/auth";
+
+
+import { PrivateRoute } from '../helpers/PrivateRoute'
 
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
@@ -9,12 +10,11 @@ import SearchPage from "./SearchPage";
 import LandingPage from "./LandingPage";
 import NewProject from "./projects/NewProject";
 import ProjectPage from "./ProjectPage";
-import UserProjectPage from "./UserProjectPage"
+import Dashboard from "./Dashboard/Dashboard"
+import Auth from './Auth'
 
 class App extends Component {
-    componentDidMount() {
-        this.props.fetchUser();
-    }
+    
     render() {
         return (
             <BrowserRouter>
@@ -23,9 +23,10 @@ class App extends Component {
                     <div className="page-content">
                         <Route exact path="/" component={LandingPage} />
                         <Route exact path="/search" component={SearchPage} />
-                        <Route exact path="/newproject" component={NewProject} />
-                        <Route exact path="/project/:id" component={ProjectPage} />
-                        <Route exact path="/project/user/projects" component={UserProjectPage}/>
+                        <Route exact path="/:username/project/:id" component={ProjectPage} />
+                        <Route exact path="/auth" component={Auth} />
+                        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                        <PrivateRoute exact path="/newproject" component={NewProject} />
                     </div>
                     <Footer />
                 </div>
@@ -34,7 +35,6 @@ class App extends Component {
     }
 }
 
-export default connect(
-    null,
-    { fetchUser }
-)(App);
+
+
+export default App;
