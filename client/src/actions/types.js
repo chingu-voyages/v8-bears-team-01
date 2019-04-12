80df7f6598009ddd65dd55e05e9e74a74ea74e7e
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-11 21:03:47 -0500

    added type to get auth user

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,4 @@ export const FETCH_USER = "fetch_user";
 export const CREATE_PROJECT = "create_project";
 export const GET_PROJECTS = "get_projects";
 export const GET_PROJECT = "get_project";
+export const GET_AUTH_TOKEN= "get_auth_token";
