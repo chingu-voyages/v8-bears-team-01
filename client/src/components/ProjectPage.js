a934329cb2c75e28ca19a3008cc424c1f79f7d01
Crystal <hello@crystalbohacek.com>
2019-04-14 18:11:58 -0400

    added edit functionality

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -2,18 +2,65 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getProject } from "../actions/project";
 import { Link } from "react-router-dom";
+import EditProject from './projects/EditProject';
 
 class ProjectPage extends Component {
   constructor (){
     super()
     this.state = {
-      success: false
+      success: false,
+      editIsActive: false,
+      project: {}
     }
+  }
+  handleEditToggle = () => {
+    this.setState(() => ({ 
+      editIsActive: !this.state.editIsActive
+    }));
+  }    
+  handleSaveProject = (deadline, description) => {
+    const id = this.props.match.params.id;
+    fetch(`/api/projects/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ description, deadline })
+    }).then(() => {
+      this.setState(() => ({ 
+        project : { deadline, description },
+        editIsActive: !this.state.editIsActive
+      }));
+    });
   }
   displaySuccessMesage = () => {
     return (
       <div class="alert alert-success" role="alert">
         Project successfully deleted. <Link to="/">Go home.</Link>
+      </div>
+    )
+  }
+  renderEdit = () => {
+    if (this.state.editIsActive){
+      return (
+        <EditProject 
+          handleSaveProject={this.handleSaveProject} 
+          handleEditToggle={this.handleEditToggle} 
+          description={this.state.project.description}
+          deadline={this.state.project.deadline}
+        />
+      )
+    }
+    return (
+      <div>
+        <p>Deadline: {this.state.project.deadline}</p>
+        <p>
+          Description: {this.state.project.description}
+        </p>
+        <button
+        onClick={this.handleEditToggle}
+        className="btn btn-sm btn-outline-light mr-1 mb-2"
+        >
+          edit
+        </button>
       </div>
     )
   }
@@ -32,11 +79,11 @@ class ProjectPage extends Component {
         .catch(err => console.log(err));
     }
   };
-  onEditButtonClick = e => {
-    
-  }
   componentDidMount() {
-    this.props.getProject(this.props.match.params.id);
+    var that = this;
+    this.props.getProject(this.props.match.params.id, function(data){
+      that.setState({ project : data })
+    });
   }
   render() {
     const { project } = this.props;
@@ -56,26 +103,17 @@ class ProjectPage extends Component {
                 <h1 className="text-light">{!!project && project.projectName ? project.projectName : ""}</h1>
                 <p className="mb-4 lead text-light">
                   Created by: {!!project && project.ownerName ? project.ownerName : ""}
-                </p>
-                <p>Deadline: {!!project && project.deadline ? project.deadline : ""}</p>
-                <p>
-                  Description: {!!project && project.description ? project.description : ""}
-                </p>
+                </p>      
+                {this.renderEdit()}
                 <button
-                onClick={this.onEditButtonClick}
-                className="btn btn-sm btn-outline-light mr-1 mb-2"
-                >
-                  edit
-                </button>
-                <button
-                  onClick={this.onDeleteButtonClick}
-                  className="btn btn-sm btn-outline-danger mb-2"
-                >
-                  delete
-                </button>
-                <button className="btn btn-teal btn-block">
-                    Apply to Project
-                </button>              
+                    onClick={this.onDeleteButtonClick}
+                    className="btn btn-sm btn-outline-danger mb-2"
+                  >
+                    delete project
+                  </button>
+                  <button className="btn btn-teal btn-block">
+                      Apply to Project
+                  </button>              
               </div>
             </div>
           </div>
