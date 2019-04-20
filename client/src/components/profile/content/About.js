4881b9b92e18888bfcfa353c76757697f1c4c8e4
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-20 17:05:09 -0500

    added content to the about component

--- client/src/components/profile/content/About.js
+++ client/src/components/profile/content/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <p>this is the about page</p>
+    <h3>About John Doe</h3>
+      <p>My name is John Doe and I am a Front End Developer. My previous expereince was working for Orange Software for 8 years and I am currently a freelance developer. For more information of my work, check out my portfolio link.</p>
     </div>
   )
 }
