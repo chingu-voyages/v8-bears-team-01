26efb72a47aed08c313491251c6750da4b867d1a
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-19 20:14:05 -0500

    added profile route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,7 @@ import NewProject from "./projects/NewProject";
 import ProjectPage from "./ProjectPage";
 import Dashboard from "./Dashboard/Dashboard";
 import Auth from "./Auth";
+import Profile from "./profile/Profile"
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
               path="/:username/project/:id"
               component={ProjectPage}
             />
+            <Route path="/profile/:username" component={Profile}/>
             <Route exact path="/auth" component={Auth} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/newproject" component={NewProject} />
