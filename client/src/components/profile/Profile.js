bc3aacf9f896d83963ac0fe9790e355c7e901126
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-20 01:32:14 -0500

    added content and the ability to switch class

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import Sidebar from './Sidebar'
+import Content from './Content'
 
 class Profile extends Component {
+    state = {
+      about: true,
+      myProject: false,
+      collaborators: false,
+      myContacts: false,
+      currentInput : 'about'
+    }
+
+    updateState = (name) =>{
+      let currentInput = this.state.currentInput
+      this.setState(prevState=>({[currentInput]:!prevState[currentInput]}))
+   
+      this.setState(prevState=>({[name]: !prevState[name],
+      
+                                currentInput: name
+                              }))
+    }
+
+    
+
   render() {
     return (
-      <>
-        <Sidebar/>
-      </>
+      <div>
+        <Sidebar {...this.state} updateState={this.updateState} updateInput={this.updateCurrentInput}/>
+        <Content />
+      </div>
     )
   }
 }
