fb9aa5b854f3c558ec4b18eedb06d2ec77975984
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-28 02:31:41 -0500

    added image upload functionality to about page

--- client/src/components/profile/content/About.js
+++ client/src/components/profile/content/About.js
@@ -1,15 +1,35 @@
 import React, {useState} from 'react';
 import Modal from 'react-modal';
+import SelectUSState from './../../../helpers/SelectUSState';
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from '../../../helpers/firebase.js';
 
 const About = () => {
   const [isOpen, setOpen] = useState(false);
+
+  const [value, updateValue] = useState('');
+  const [avatarURL, setAvatarURL] = useState('');
 
   const updateModal = () => {
     setOpen(true);
   };
 
+  const handleUploadSuccess = (filename) => {
+    // this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref('photos')
+      .child(filename)
+      .getDownloadURL()
+      .then((url) => setAvatarURL(url));
+  };
+
   const handleRequestClose = () => {
     setOpen(false);
+  };
+
+  const setNewValue = ({target: {value}}) => {
+    updateValue(value);
   };
 
   return (
@@ -21,18 +41,63 @@ const About = () => {
         onRequestClose={handleRequestClose}
         ariaHideApp={false}
         closeTimeoutMS={300}>
-      >
-      <div className="editprofile-modal">
-      <input
-      className="form-control"
-      placeholder="Name"
-    />
+        <div className="editprofile-modal">
+          <button
+            className="btn btn-link nav-link text-light close-button profile-close"
+            onClick={handleRequestClose}>
+            <i className="fas fa-times" />
+          </button>
+          <div className="profile-picture">
+            {!!avatarURL ? (
+              <img
+                src={avatarURL}
+                className="profile-pricture"
+                alt="profile picture"
+              />
+            ) : (
+              <i className="fas fa-user-circle fa-7x" />
+            )}
+
+            <label className="profile-upload-button">
+              <i className="fas fa-pencil-alt fa-2x">
+                <FileUploader
+                  hidden
+                  accept="image/*"
+                  name="avatar"
+                  randomizeFilename
+                  storageRef={firebase.storage().ref('photos')}
+                  onUploadSuccess={handleUploadSuccess}
+                />
+              </i>
+            </label>
+          </div>
+          <div className="profile-button-input-container">
+            <input
+              className="form-control profile-input-control"
+              placeholder="Name"
+            />
+            <textarea
+              className="form-control profile-input-control"
+              rows="5"
+              placeholder="About yourself"
+            />
+            <input
+              className="form-control profile-input-control"
+              placeholder="Job Title"
+            />
+            <div className="profile-location">
+              <p htmlFor="state">Location:</p>
+
+              <SelectUSState
+                className="form-control profile-input-control"
+                value={value}
+                handleChange={setNewValue}
+              />
+            </div>
+          </div>
           <div className="profile-button-container">
             <a onClick={updateModal} className="profile-button">
               Update
-            </a>
-            <a onClick={handleRequestClose} className="profile-button">
-              Close
             </a>
           </div>
         </div>
