f40505534e86915c1746884711f92d5315449666
Daniel Ko <danko56666@gmail.com>
2019-04-29 16:05:30 -0400

    modified config

--- client/src/helpers/firebase.js
+++ client/src/helpers/firebase.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase';
-import {APIKEY, MESSAGINGSENDERID} from './key'
+import firebase from "firebase";
 
-let config ={
-  apiKey: APIKEY,
+let config = {
+    apiKey: "AIzaSyD_cY4Wz7rzrC4SbrzVhi2J51XRM3tpPYI",
     authDomain: "timeline-blog.firebaseapp.com",
     databaseURL: "https://timeline-blog.firebaseio.com",
     projectId: "timeline-blog",
     storageBucket: "timeline-blog.appspot.com",
-    messagingSenderId: MESSAGINGSENDERID
-}
+    messagingSenderId: "964075740874"
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 export default firebase;
