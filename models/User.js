7243b1ddb2b544798fcfe2cb4bf41f68a696a7cb
Owen Ekhator <owenekhator@rocketmail.com>
2019-04-30 22:45:49 -0500

    updated user model

--- models/User.js
+++ models/User.js
@@ -19,6 +19,18 @@ const userSchema = new Schema({
     picture: {
         type: String,
         required: false
+    },
+    about: {
+        type: String,
+        required: false
+    },
+    job_title:{
+        type: String,
+        required: false
+    },
+    location:{
+        type: String,
+        required: false
     }
 });
 
